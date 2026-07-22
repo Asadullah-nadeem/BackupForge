6c4b104c609d8973307f3b149af806ea2ac16f84
Asadullah Nadeem <88024587+Asadullah-nadeem@users.noreply.github.com>
2026-07-22 17:49:44 +0530

    Remove mock template sidebar layout and preserve native BackupForge dashboard

--- frontend/src/app/(DashboardLayout)/layout.tsx
+++ frontend/src/app/(DashboardLayout)/layout.tsx
@@ -1,32 +1,7 @@
-'use client'
-
-import Header from './layout/header/Header'
-import Sidebar from './layout/sidebar/Sidebar'
-
 export default function Layout({
   children,
 }: Readonly<{
   children: React.ReactNode
 }>) {
-  return (
-    <>
-      <div className='flex w-full min-h-screen'>
-        <div className='page-wrapper flex w-full'>
-          {/* Header/sidebar */}
-          <div className='xl:block hidden'>
-            <Sidebar />
-          </div>
-
-          <div className='body-wrapper w-full'>
-            {/* Top Header  */}
-            <Header />
-            {/* Body Content  */}
-            <div className="bg-lightgray dark:bg-dark mr-3 rounded-3xl min-h-[90vh]">
-              <div className={`container mx-auto px-6 py-30`}>{children}</div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </>
-  )
+  return <div className="min-h-screen w-full bg-[#0f172a]">{children}</div>;
 }
